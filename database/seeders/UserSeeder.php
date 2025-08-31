d5e8ee716d21e2419410f3637a368677695be997
Rifkyrahmat2006 <rifkyrahmat2006@gmail.com>
2025-08-31 22:33:03 +0700

    feat: login

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Models\Role;
 class UserSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds
      */
     public function run(): void
     {
---
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -0,0 +1,37 @@
+<x-filament-panels::page.simple>
+    {{-- Tombol Back --}}
+    <div class="mb-6">
+        <a 
+            href="{{ url('/') }}" 
+            class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-gray fi-btn-color-gray fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-white text-gray-950 hover:bg-gray-50 focus-visible:ring-gray-500/50 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:focus-visible:ring-gray-400/50 fi-ac-action fi-ac-btn-action"
+        >
+            <svg class="fi-btn-icon transition duration-75 h-5 w-5 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clip-rule="evenodd"/>
+            </svg>
+            <span class="fi-btn-label">
+                Kembali ke Beranda
+            </span>
+        </a>
+    </div>
+
+    @if (filament()->hasRegistration())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+
+            {{ $this->registerAction }}
+        </x-slot>
+    @endif
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE, scopes: $this->getRenderHookScopes()) }}
+
+    <x-filament-panels::form id="form" wire:submit="authenticate">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+    </x-filament-panels::form>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
+</x-filament-panels::page.simple>
