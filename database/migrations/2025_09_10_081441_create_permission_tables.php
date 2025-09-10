b7672e1317fa6210281c6af38248bef283356dd2
Rifkyrahmat2006 <rifkyrahmat2006@gmail.com>
2025-09-10 11:25:01 +0700

    ulid sync

--- database/migrations/2025_09_10_081441_create_permission_tables.php
+++ database/migrations/2025_09_10_081441_create_permission_tables.php
@@ -50,9 +50,7 @@ return new class extends Migration
         Schema::create($tableNames['model_has_permissions'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission, $teams) {
             $table->unsignedBigInteger($pivotPermission);
 
-            $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
-            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
+            $table->ulidMorphs('model'); // model_id = CHAR(26), + index
 
             $table->foreign($pivotPermission)
                 ->references('id') // permission id
@@ -62,10 +60,10 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                $table->primary([$columnNames['team_foreign_key'], $pivotPermission, 'model_id', 'model_type'],
                     'model_has_permissions_permission_model_type_primary');
             } else {
-                $table->primary([$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                $table->primary([$pivotPermission, 'model_id', 'model_type'],
                     'model_has_permissions_permission_model_type_primary');
             }
 
@@ -74,9 +72,7 @@ return new class extends Migration
         Schema::create($tableNames['model_has_roles'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole, $teams) {
             $table->unsignedBigInteger($pivotRole);
 
-            $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
-            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
+            $table->ulidMorphs('model'); // model_id = CHAR(26), + index
 
             $table->foreign($pivotRole)
                 ->references('id') // role id
@@ -86,10 +82,10 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                $table->primary([$columnNames['team_foreign_key'], $pivotRole, 'model_id', 'model_type'],
                     'model_has_roles_role_model_type_primary');
             } else {
-                $table->primary([$pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                $table->primary([$pivotRole, 'model_id', 'model_type'],
                     'model_has_roles_role_model_type_primary');
             }
         });
