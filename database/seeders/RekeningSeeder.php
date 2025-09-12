0aedaf54638d2a42de62e473e6c74b12ae811c50
Karel <karelriyan@gmail.com>
2025-09-12 20:33:30 +0700

    Feat: rekening bank sampah

---
+++ database/seeders/RekeningSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Rekening;
+use Illuminate\Support\Carbon;
+
+class RekeningSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Menggunakan updateOrCreate untuk mencegah duplikasi jika seeder dijalankan lagi
+        // Kunci pencarian adalah 'no_rekening'
+        Rekening::updateOrCreate(
+            ['no_rekening' => '00000000000000'],
+            [
+                'nama'          => 'Tabungan Bank Sampah',
+                'dusun'         => '-',
+                'rt'            => '-',
+                'rw'            => '-',
+                'gender'        => '-',
+                'no_kk'         => '0000000000000000', // Harus unik
+                'nik'           => '0000000000000000', // Harus unik dan berbeda dari no_kk
+                'tanggal_lahir' => Carbon::now(),
+                'pendidikan'    => '-',
+                'balance'       => 0, // Saldo awal
+                'points_balance'=> 0, // Poin awal
+            ]
+        );
+    }
+}
