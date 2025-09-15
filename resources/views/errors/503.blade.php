67e5847aa25aadf0fdb51577ace0ce73ca863579
Karel <karelriyan@gmail.com>
2025-09-15 15:30:14 +0700

    Feat: custom eror

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::minimal')
+
+@section('title', __('Perbaikan'))
+@section('code', '503')
+@section('message')
+    {!! __('Maaf, CiptaMuri sedang dalam perbaikan<br>Silakan hubungi Karel atau Rifky.') !!}
+@endsection
